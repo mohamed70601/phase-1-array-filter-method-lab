9b201b838b1ceff0e84be5f4f17834c0dc30df21
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-08-12 00:59:32 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+function findMatching(driversArr, string) {
+    const lowerSearch = string.toLowerCase();
+    const matchingDrivers = driversArr.filter(driver =>
+        driver.toLowerCase().includes(lowerSearch)
+    );
+    return matchingDrivers;
+}
+
+function fuzzyMatch(driversArr, string) {
+    const lowerSearch = string.toLowerCase();
+    const matchingDrivers = driversArr.filter(driver =>
+        driver.toLowerCase().startsWith(lowerSearch)
+    );
+    return matchingDrivers;
+}
+
+function matchName(driversArr, string) {
+    const matchingDrivers = driversArr.filter(driver => driver.name === string);
+    return matchingDrivers;
+}
